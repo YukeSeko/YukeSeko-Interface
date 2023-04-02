3b8638a05d3265c3c3265dee4fe0932e7464b076
wzy <1286694258@qq.com>
2023-04-02 17:26:15 +0800

    修改Redissen线上配置

--- api/src/main/java/com/wzy/api/config/RedissonConfig.java
+++ api/src/main/java/com/wzy/api/config/RedissonConfig.java
@@ -3,6 +3,7 @@ package com.wzy.api.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,11 +14,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RedissonConfig {
 
+    @Value("${spring.redis.password}")
+    private String redisPassword;
+
     @Bean(destroyMethod = "shutdown")
     public RedissonClient redisson(){
         Config config = new Config();
         config.useSingleServer().setAddress("redis://localhost:6379");
-        config.useSingleServer().setPassword("password");
+        config.useSingleServer().setPassword(redisPassword);
         return Redisson.create(config);
     }
 }
